3a9242c81889aa9df10360b2bc4bd59ae5921ea1
Yuji Kiriki <yujikirki@gmail.com>
2017-03-23 16:54:21 -0500

    AAAAAA

--- src/test/scala/co/s4n/MainTest.scala
+++ src/test/scala/co/s4n/MainTest.scala
@@ -8,5 +8,26 @@ class MainTest extends FunSuite {
     info("Hello world")
     assert("Hello world" === Main.m())
   }
+
+  test("No se manejar Either[T]") {
+    val derecha: Either[String, String] = Right("Yujito")
+    val izquierda: Either[String, String] = Left("Yujito")
+
+    val o: Option[String] = Option("Hola")
+
+    /*
+    val a: Right[String] = izquierda.flatMap {
+      x => x
+    }
+
+    val d: (Either[String, String], Either[String, String]) = for { 
+      i <- izquierda
+      d <- derecha 
+    } yield (i, d)
+    
+    assert(d === Left("Yujito"))
+
+    */
+  }
 }
 
